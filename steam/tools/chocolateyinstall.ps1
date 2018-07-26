f22f828f5213732fd28f959a76916b4e0c8cf2c9
Austin Proctor <ajhproctor@gmail.com>
2018-07-25 22:42:23 -0700

    Added steam package

---
+++ steam/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$url = 'https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe'
+$silentArgs = '/S'
+$requestor = [System.Web.WebRequest]::Create($url)
+
+if ($requestor.GetResponse().StatusCode -ne 'OK') {
+  Write-Output "$url is unreachable. Installation failed."
+  exit 1603
+}
+
+$installChocoPkg = @{
+  PackgeName = $env:ChocolateyPackageName
+  FileType = 'exe'
+  SilentArgs = $silentArgs
+  Url = $url
+  ValidExitCodes = 0
+}
+
+Install-ChocolateyPackage @installChocoPkg
