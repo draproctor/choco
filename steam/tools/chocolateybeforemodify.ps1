10ef1812bbead19c029d98966f005578914daaf5
Austin Proctor <ajhproctor@gmail.com>
2018-07-25 22:54:02 -0700

    Set up pre-install and install scripts

--- steam/tools/chocolateybeforemodify.ps1
+++ steam/tools/chocolateybeforemodify.ps1
@@ -1,9 +1,16 @@
-﻿# This runs in 0.9.10+ before upgrade and uninstall.
-# Use this file to do things like stop services prior to upgrade or uninstall.
-# NOTE: It is an anti-pattern to call chocolateyUninstall.ps1 from here. If you
-#  need to uninstall an MSI prior to upgrade, put the functionality in this
-#  file without calling the uninstall script. Make it idempotent in the
-#  uninstall script so that it doesn't fail when it is already uninstalled.
-# NOTE: For upgrades - like the uninstall script, this script always runs from 
-#  the currently installed version, not from the new upgraded package version.
+﻿# Stop any potential Steam services, though there shouldn't be any.
+# If the processes get stuck, stop this install.
+$ErrorActionPreference = 'Stop'
 
+$processNames = @(
+  'Steam'
+  'SteamService'
+  'steamwebhelper'
+  'GameOverlayUI'
+)
+
+$steamService = 'Steam Client Service'
+
+$processNames | Stop-Process -Force
+Stop-Service -Name $steamService -Force
+Set-Service -Name $steamService -StartupType Disabled
